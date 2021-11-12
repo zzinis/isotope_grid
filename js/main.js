e0584b9cf0436ba8e9d22af923995ee30cad2ea3
zzinis <lizzyin@naver.com>
2021-11-12 17:24:35 +0900

    header 추가

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var btnCall = document.querySelector(".btnCall"); 
+
+var menuMo = document.querySelector(".menuMo"); 
+var btnClose = document.querySelector(".btnClose");
+
+btnCall.onclick = function(e){
+    e.preventDefault(); 
+    
+    btnCall.classList.toggle("on"); 
+    menuMo.classList.toggle("on"); 
+}
