a07ecc80cb56796e1d679f9dfc7462d648d7c388
zzinis <lizzyin@naver.com>
2021-11-12 16:35:04 +0900

    grid layout 수정

--- js/isotope-sizer.js
+++ js/isotope-sizer.js
@@ -1,8 +1,16 @@
-new Istotope("section",{
-    itemSelector: ".item",
-    precentPosition: true,
-    transitionDuraition: "0.8s",
-    masonry:{
-        columWidth: ".item-sizer"
+// new Isotope("section",{
+//     itemSelector: ".item",
+//     precentPosition: true,
+//     transitionDuraition: "0.8s",
+//     masonry:{
+//         columWidth: ".item-sizer"
+//     }
+// })
+$('section').isotope({
+    itemSelector: '.item',
+    percentPosition: true,
+    masonry: {
+      columnWidth: '.item-sizer'
     }
-})
+  });
+  
